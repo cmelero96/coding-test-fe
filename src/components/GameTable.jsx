31ece031c9494ee6d84d5c28128b058aa4f00fc2
Carlos Melero <cmelero96@gmail.com>
2022-04-24 18:31:37 +0200

    Add WS client to component

--- src/components/GameTable.jsx
+++ src/components/GameTable.jsx
@@ -1,11 +1,25 @@
 import React, {useEffect, useState} from 'react';
 
+import useWsClient from '../hooks/useWsClient';
+import { ENDPOINTS } from '../constants/server';
+
 const GameTable = ({tournaments}) => {
   const [localTournaments, setLocalTournaments] = useState([]);
+
+  const { newMessage } = useWsClient(ENDPOINTS.series);
 
   useEffect(() => {
     setLocalTournaments(tournaments);
   }, [tournaments]);
+
+  useEffect(() => {
+    newMessage.forEach(match => {
+      const tournament = localTournaments.find(t => t.id === match.id);
+
+      tournament.match = {...match}
+    })
+
+  }, [localTournaments, newMessage, tournaments])
 
   return (
     <div>
